b1976f6636cad05a814bc62c2135bf4d0c2875fd
Vitaliy Zadorozhniy <zadoroshniy1@gmail.com>
2026-01-06 23:35:16 +0400

    Lesson 7. ORM

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,15 +1,27 @@
 from django.db import models
-from django.utils import timezone
 from django.utils.text import slugify
-
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Article(models.Model):
-    name = models.fields.CharField(max_length=100, null=False)
-    text = models.fields.TextField()
-    date_created = models.fields.DateTimeField(default=timezone.now())
+    name = models.CharField(max_length=100, null=False)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name="ASDA")
     slug = models.SlugField(default="", null=True, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
         super(Article, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.slug
+
+
+class UserFeedback(models.Model):
+    username = models.CharField(max_length=128)
+    phone = PhoneNumberField()
+    email = models.EmailField()
+    text = models.TextField()
+
+    def __str__(self):
+        return f"Client {self.username} + {self.email}"
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from .models import Article
+from .models import Article, UserFeedback
 from datetime import datetime
 
 class ArticlesGeneric(ListView):
@@ -13,6 +13,18 @@ class ArticlesGeneric(ListView):
 class ArticleDetailGeneric(DetailView):
     model = Article
 
+
+def process_feedback(request):
+    text = "Hello, my company Tesla got troubles with AI optimizations"
+    user_feedback = UserFeedback(
+        username="Oleg",
+        email="asda@gmail.com",
+        phone="+380979132112",
+        text=text
+    )
+    user_feedback.save()
+    if generate_and_append_pain_poins(user_feedback):
+        user_feedback.refresh_from_db()
 
 
 
